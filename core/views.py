c5b130dd778787d1bc1c39431f7aec8525d8b6e0
Rafael <rafaelpereiradearaujo5@gmail.com>
2025-10-28 01:46:29 -0300

    feat: Implement home and dashboard pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+
+class DashboardPageView(LoginRequiredMixin, TemplateView):
+    """
+    View principal do sistema após o login (RF005).
+    """
+    template_name = "core/dashboard.html"
